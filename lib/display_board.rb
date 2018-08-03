aca302026181ffe30f2a1a27f9f84248a729b1b7
ktochow1 <ktochow1@binghamton.edu>
2018-08-03 13:55:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -23,3 +23,15 @@ puts "#{board[6]}  |#{board[7]}  |#{board[8]}  "
 
 end
 display_board(board)
+
+
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+puts "#{board[0]}  |#{board[1]}  |#{board[2]}  "
+puts "-----------"
+puts "#{board[3]}  | #{board[4]} |#{board[5]}  "
+puts "-----------"
+puts "#{board[6]}  |#{board[7]}  |#{board[8]}  "
+
+end
+display_board(board)
